6a8bb321db7bdce22f70a7e306716bd1a351b813
victxrfreitas <maciel.freitas@sptech.school>
2022-06-03 14:14:06 -0300

    Pagina usuario, votação e outros acertos

---
+++ src/database/script-kbling.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE kbling;
+
+USE kbling;
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+    fkAlbum INT,
+		FOREIGN KEY (fkAlbum) references album(idAlbum)
+);
+
+CREATE TABLE album (
+idAlbum INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45)
+);
+
+insert into album (nome) values
+('Replay'),
+('The SHINee World'),
+('ROMEO'),
+('2009, Year Of Us'),
+('Lucifer'),
+('Sherlock'),
+('Misconceptions Of Us'),
+('Everybody'),
+('Odd'),
+('1 Of 1'),
+('The Story Of Light'),
+("Don't Call Me");
+
+select * from album;
